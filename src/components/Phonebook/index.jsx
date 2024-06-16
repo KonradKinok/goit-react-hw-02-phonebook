b71574048562919f77b5b65100ef91a4fa4dda82
Konrad Konik <88903994+KonradKinok@users.noreply.github.com>
2024-06-16 12:50:57 +0200

    16.06.2024

--- src/components/Phonebook/index.jsx
+++ src/components/Phonebook/index.jsx
@@ -1,204 +1,68 @@
-import { Children, Component } from "react";
+import { Component } from "react";
 import PropTypes from "prop-types";
-import { nanoid } from 'nanoid'
 import scss from "./Phonebook.module.scss";
-import buttonScss from "../../globalStyles/buttonStyles.module.scss";
-
-export const ButtonControl = ({ changeValue, label, style, name }) => (
-    <button name={name} className={style} type="button" onClick={changeValue}>
-        {label}
-    </button>
-);
-
-ButtonControl.propTypes = {
-    changeValue: PropTypes.func.isRequired,
-    label: PropTypes.string.isRequired,
-    style: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-};
+import ContactForm from "../ContactForm";
+import Filter from "../Filter";
+import ContactList from "../ContactList";
 
 export class Contacts extends Component {
+    static propTypes = {
+        contacts: PropTypes.arrayOf(
+            PropTypes.shape({
+                id: PropTypes.string.isRequired,
+                name: PropTypes.string.isRequired,
+                number: PropTypes.string.isRequired,
+            })
+        ),
+        filter: PropTypes.string,
+        addContact: PropTypes.func,
+        handleDelete: PropTypes.func,
+    };
+
     constructor() {
         super();
         this.state = {
-            contacts: [],
-            name: "",
-            number: "",
+            contacts: [{ id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+            { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+            { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+            { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+            { id: 'id-5', name: 'Konrad', number: '2' },],
             filter: "",
         };
     }
-
-    handleChange = (ev) => {
-        const { name, value } = ev.currentTarget;
-        this.setState({
-            [name]: value,
-        });
+    addContact = (contact) => {
+        this.setState((prevState) => ({
+            contacts: [...prevState.contacts, contact],
+        }));
     };
 
-    handleSubmit = (ev) => {
-        ev.preventDefault();
-        this.setState((prev) => {
-            const list = [...prev.contacts];
-            list.push({
-                id: nanoid(),
-                name: this.state.name,
-                number: this.state.number,
-            });
-            return { contacts: list };
-        });
+    handleDelete = (contactId) => {
+        this.setState((prevState) => ({
+            contacts: prevState.contacts.filter(
+                (contact) => contact.id !== contactId,
+            ),
+        }));
     };
-
+    handleFilterChange = (event) => {
+        this.setState({ filter: event.target.value });
+    };
     render() {
+        const { filter, contacts } = this.state;
         return (
             <div className={scss.phonebookContainer}>
                 <h1>Phonebook</h1>
-                <ContactForm />
+                <ContactForm addContact={this.addContact} contacts={contacts} />
 
                 <h2>Contacts</h2>
-                {/* <Filter ... />
-            <ContactList ... /> */}
+                <Filter filter={filter} onFilterChange={this.handleFilterChange} />
+                <ContactList contacts={contacts}
+                    filter={filter}
+                    onDelete={this.handleDelete}
+                />
             </div>
         )
     }
 }
 
-class ContactForm extends Component {
-    constructor() {
-        super();
-        this.state = {
-            contacts: [],
-            name: "",
-            number: "",
-            filter: "",
-        };
-    }
 
-    handleChange = (ev) => {
-        const { name, value } = ev.currentTarget;
-        this.setState({
-            [name]: value,
-        });
-    };
 
-    handleSubmit = (ev) => {
-        ev.preventDefault();
-        this.setState((prev) => {
-            const list = [...prev.contacts];
-            list.push({
-                id: nanoid(),
-                name: this.state.name,
-                number: this.state.number,
-            });
-            return { contacts: list };
-        });
-    };
-
-    render() {
-        const nameId = nanoid();
-        const numId = nanoid();
-        const searchId = nanoid();
-        return (
-            <>
-                <form className={scss.form} onSubmit={this.handleSubmit}>
-                    <label htmlFor={nameId}>Name</label>
-                    <input
-                        id={nameId}
-                        type="text"
-                        name="name"
-                        pattern="^[a-zA-Za]+(([' -][a-zA-Za ])?[a-zA-Za]*)*$"
-                        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                        required
-                        value={this.state.name}
-                        onChange={this.handleChange}
-                    />
-                    <label htmlFor={numId}>Phone number</label>
-                    <input
-                        id={numId}
-                        type="tel"
-                        name="number"
-                        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-                        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-                        required
-                        value={this.state.number}
-                        onChange={this.handleChange}
-                    />
-                    <button type="submit">Add contact</button>
-                </form>
-                <h1>Contacts</h1>
-                <label htmlFor={searchId}>Find contact</label>
-                <input
-                    type="text"
-                    id={searchId}
-                    name="filter"
-                    value={this.state.filter}
-                    onChange={this.handleChange}
-                />
-                <ul className={scss.list}>
-                    {this.state.contacts
-                        .filter((el) =>
-                            el.name.toLowerCase().includes(this.state.filter.toLowerCase()),
-                        )
-                        .map((contact) => (
-                            <li key={contact.id}>
-                                {contact.name} - {contact.number}
-                            </li>
-                        ))}
-                </ul>
-            </>
-        );
-    }
-}
-
-class ContactList extends Component {
-    constructor() {
-        super();
-        this.state = {
-            contacts: [],
-            name: "",
-            number: "",
-            filter: "",
-        };
-    }
-
-    handleChange = (ev) => {
-        const { name, value } = ev.currentTarget;
-        this.setState({
-            [name]: value,
-        });
-    };
-
-    handleSubmit = (ev) => {
-        ev.preventDefault();
-        this.setState((prev) => {
-            const list = [...prev.contacts];
-            list.push({
-                id: nanoid(),
-                name: this.state.name,
-                number: this.state.number,
-            });
-            return { contacts: list };
-        });
-    };
-
-    render() {
-        const nameId = nanoid();
-        const numId = nanoid();
-        const searchId = nanoid();
-        return (
-            <>
-
-                <ul className={scss.list}>
-                    {this.state.contacts
-                        .filter((el) =>
-                            el.name.toLowerCase().includes(this.state.filter.toLowerCase()),
-                        )
-                        .map((contact) => (
-                            <li key={contact.id}>
-                                {contact.name} - {contact.number}
-                            </li>
-                        ))}
-                </ul>
-            </>
-        );
-    }
-}
